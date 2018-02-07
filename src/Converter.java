58fc6a114ad1337860c9840767df52e88cf60b3c
Stefan Njegomirovic <stefannjegomirovic@gmail.com>
2018-02-07 14:45:28 +0100

    Univerzalni konvertor

---
+++ src/Converter.java
@@ -0,0 +1,33 @@
+
+public class Converter {
+
+	// metoda koja konvertuje kilograme u funte
+	public static double kilogramiUfunte(double kg) {
+		return 0.0;
+	}
+
+	// metoda koja konvertuje funte u kilograme
+	public static double funteUkilograme(double pounds) {
+		return 0.0;
+	}
+
+	// metoda koja konvertuje centimetre u ince
+	public static double centimetriUince(double cm) {
+		return 0.0;
+	}
+
+	// metoda koja konvertuje ince u centimetre
+	public static double inciUcentimetre(double inches) {
+		return 0.0;
+	}
+
+	// metoda koja konvertuje celzijuse u farenhajte
+	public static double celzijiUfarenhajte(double celziji) {
+		return 0.0;
+	}
+
+	// metoda koja konvertuje farehajtre u celzijuse
+	public static double farenhajtiUcelzije(double pounds) {
+		return 0.0;
+	}
+}
